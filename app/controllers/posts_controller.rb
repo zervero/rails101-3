8d8fecd16db27f9a8b788e6faddb2b1e91bec7e8
zengxiangrong <zengxiangrong5261435@gmail.com>
2016-12-31 16:32:12 +0800

    implement delete and eidt posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,23 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post  = Post.find(params[:id])
+  end
+
+  def update
+    @post  = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to account_posts_path , notice: "Update Success"
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path , notice: "Delete Success"
+  end
+
   private
   def post_params
     params.require(:post).permit(:content)
