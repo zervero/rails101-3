ea9ae7e0170a0adc79e8c0d89c1a2b5d89ee834e
zengxiangrong <zengxiangrong5261435@gmail.com>
2016-12-31 10:26:19 +0800

    implement is_member_of?(group)

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,5 +3,5 @@ class Group < ApplicationRecord
 	validates :title, presence: true
 	has_many :posts
 	has_many :group_relationships
-	has_many :members, through:group_relationships, source: :user 
+	has_many :members, through: :group_relationships, source: :user 
 end
